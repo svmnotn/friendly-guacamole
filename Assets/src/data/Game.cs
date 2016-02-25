98e6f40fbf690eb14e95be6306561ca7454806e7
Victor M. Suarez <svmnotn@gmail.com>
2016-02-25 11:10:24 -0500

    feat: add constructor to Game.cs

--- Assets/src/data/Game.cs
+++ Assets/src/data/Game.cs
@@ -5,5 +5,10 @@ public class Game {
   Player curr;
   Dictionary<Vector, Grid> map;
 
-
+  public Game(List<Player> players) {
+	  this.players = players;
+	  curr = players.ToArray () [0];
+	  map = new Dictionary<Vector, Grid> ();
+	  map.Add (new Vector (0, 0), new Grid ());
+  }
 }
