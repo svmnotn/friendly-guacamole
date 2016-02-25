82b69a6d844a5eb4def80d48ec10d2baa80c9d6d
Victor M. Suarez <svmnotn@gmail.com>
2016-02-25 11:15:38 -0500

    feat: create game in the MapController

--- Assets/src/controllers/MapController.cs
+++ Assets/src/controllers/MapController.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MapController : MonoBehaviour {
-  Game game;
+   Game game;
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+  void Start () {
+    var players = new List<Player>();
+    // TODO Take in all the players
+    players.Add (new Player("X"));
+    players.Add (new Player("O"));
+    game = new Game (players);
+  }
+
+  void Update () {
+    Win ();
+  }
+
+  void Win() {
+    var win = game.check ();
+    if (win.win) {
+      //TODO Win Game
+    }
+  }
 }
